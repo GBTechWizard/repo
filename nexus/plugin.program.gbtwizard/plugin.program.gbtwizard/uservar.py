07c23d2b1bac6d59d25f8618014c1fd4781a78c0
GBTech <gbtechwizard@gmail.com>
2023-04-10 13:19:12 +0100

    builds.xml

--- nexus/plugin.program.gbtwizard/plugin.program.gbtwizard/uservar.py
+++ nexus/plugin.program.gbtwizard/plugin.program.gbtwizard/uservar.py
@@ -1,7 +1,7 @@
 import xbmcaddon
 
 '''#####-----Build File-----#####'''
-buildfile = 'https://raw.githubusercontent.com/GBTechWizard/repo/master/nexus/wizard/builds.txt'
+buildfile = 'https://raw.githubusercontent.com/GBTechWizard/repo/master/nexus/wizard/builds.xml'
 
 '''#####-----Notifications File-----#####'''
 notify_url  = 'https://raw.githubusercontent.com/GBTechWizard/repo/master/nexus/wizard/notify.txt'

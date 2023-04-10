3ce3d900bde63ebd5f1d1c011f33c7ea25c88d79
GBTech <gbtechwizard@gmail.com>
2023-04-10 12:46:01 +0100

    New Nexus Folder
    Actualização para Kodi Nexus

---
+++ nexus/plugin.program.gbtwizard/plugin.program.gbtwizard/uservar.py
@@ -0,0 +1,10 @@
+import xbmcaddon
+
+'''#####-----Build File-----#####'''
+buildfile = 'https://raw.githubusercontent.com/GBTechWizard/repo/master/nexus/wizard/builds.txt'
+
+'''#####-----Notifications File-----#####'''
+notify_url  = 'https://raw.githubusercontent.com/GBTechWizard/repo/master/nexus/wizard/notify.txt'
+
+'''#####-----Excludes-----#####'''
+excludes  = ['plugin.video.whatever']
